ed736a5f2dfa3d4643ca5a3ed388b34a475951a8
Mauro Clavijo <maroclavijoh@yahoo.com>
2021-07-18 23:37:32 -0500

    First Version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Table from './Table.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container,Row,Col} from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        id_order : "",
+          details: [],
+          order:[]
+      }
+  }
+
+  handleChange = (e) => {
+  this.setState({ id_order: e.target.value });  // This will update the input value in state
+  }
+
+  doingASearch=(event)=>{
+    event.preventDefault();
+  	fetch('http://localhost:8080/orders/'+this.state.id_order+'/details')
+  	.then(res => res.json())
+  	.then(details => this.setState({ 'details': details }))
+
+    fetch('http://localhost:8080/orders/'+this.state.id_order)
+  	.then(res => res.json())
+  	.then(order => this.setState({ 'order': order }))
+    }
+
+
+
+  render() {
+    return (
+        <div className="App">
+          <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="./">
+               Detalles de Orden
+            </a>
+          </nav>
+          <form style={{ marginTop: "1em" }} onSubmit={this.doingASearch}>
+            <label htmlFor="searchTerm">
+              <strong>Id de Orden </strong>
+              <input type="text" name="id_order" onChange={this.handleChange}/>
+              <button type="submit" class="btn btn-light">Buscar</button>
+            </label>
+          </form>
+          <Container style={{ margin: "1em" }}>
+            <Row>
+              <Col sm>Cliente:{this.state.order.client}</Col>
+              <Col sm>N Orden:{this.state.order.number}</Col>
+              <Col sm>Total:${this.state.order.total}</Col>
+            </Row>
+          </Container>
+          <Table details={ this.state.details } />
+        </div>
+      );
+    }
 }
 
 export default App;
---
+++ src/Table.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Table = ({ details }) => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Num</th>
+          <th>Descripcion</th>
+          <th>Cantidad</th>
+          <th>Precio Unitario</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+      <tbody>
+      { (details.length > 0) ? details.map( (droplet, index) => {
+        return (
+          <tr key={ index }>
+            <td>{ index+1 }</td>
+            <td>{ droplet.detail }</td>
+            <td>{ droplet.cantidad}</td>
+            <td>{ droplet.precioUnitario }</td>
+            <td>{ droplet.totalDetail }</td>
+          </tr>
+        )
+      }) : <tr><td colSpan="5">Inserte un valor</td></tr> }
+      </tbody>
+    </table>
+  );
+}
+
+export default Table
